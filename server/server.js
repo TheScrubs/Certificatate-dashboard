d674e8e1dcd127a0389566f226631140282c337d
limZyuan <58379837+limZyuan@users.noreply.github.com>
2021-05-20 17:40:23 +0800

    slight edits

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const path = require("path");
+
+//config dot env
+require("dotenv").config();
+
+// Initialise express app
 const app = express();
+
+// Body Parser Middleware
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false }));
 
 // set static folder
 app.use(express.static(path.join(__dirname, "../", "client", "public")));
