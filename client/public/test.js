6747c284afcdb7957b3e4fae7561753fcceb6163
Quinn Cheong <quinncheong@hotmail.com>
2021-05-20 14:28:07 +0800

    colour scheme 2

--- client/public/test.js
+++ client/public/test.js
@@ -1,6 +1,6 @@
 // This is a test file for logging inside the console.
 
-var gg = document.getElementById('gg')
+// var gg = document.getElementById('gg')
 
-console.log(gg.innerHTML);
-console.log(gg.textContent);
+// console.log(gg.innerHTML);
+// console.log(gg.textContent);
