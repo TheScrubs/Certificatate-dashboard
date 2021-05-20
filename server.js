60baea966b81bc8afd89e606accf445204aa2c79
limZyuan <58379837+limZyuan@users.noreply.github.com>
2021-05-20 16:50:19 +0800

    updates

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const http = require("http");
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+
 const app = express();
 
-const server = http.createServer(app);
-
+// set static folder
 app.use(express.static(path.join(__dirname, "client", "public")));
 
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
