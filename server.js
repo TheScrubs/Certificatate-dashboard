1b658b53599b8ea83302a5495ac120ec83d52cf7
Quinn Cheong <quinncheong@hotmail.com>
2021-05-20 14:27:53 +0800

    Colour scheme sorted out

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 const app = express();
 
 const server = http.createServer(app);
