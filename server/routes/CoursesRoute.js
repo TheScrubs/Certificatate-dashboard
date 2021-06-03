a186ae1585d8d824ddfd72f423caf1f12e480c66
limZyuan <58379837+limZyuan@users.noreply.github.com>
2021-06-03 15:52:05 +0800

    coursera data endpoint

--- server/routes/CoursesRoute.js
+++ server/routes/CoursesRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const UdemyCoursesModel = require("../models/udemyCoursesSchema.js");
+const CourseraCoursesModel = require("../models/courseraCoursesSchema.js");
 
 const router = express.Router();
 
@@ -16,6 +17,18 @@ router.post("/getUdemyCourses", (req, res, next) => {
       UdemyObj[course._id] = course;
     });
     return res.json(UdemyObj);
+  });
+});
+
+//get all udemy courses from db collection
+router.post("/getCourseraCourses", (req, res, next) => {
+  var CourseraObj = {};
+
+  CourseraCoursesModel.find({}).then((courses) => {
+    courses.map((course) => {
+      CourseraObj[course._id] = course;
+    });
+    return res.json(CourseraObj);
   });
 });
 
