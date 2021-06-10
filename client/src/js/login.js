c13e716e8081b83b6d90f6ec9e180db644c4c63b
Potokiller <enversionkiller@gmail.com>
2021-06-10 13:58:23 +0800

    added theme-selector for login.html, functionality issues remain, color-schemes still need re-adjustments

--- client/src/js/login.js
+++ client/src/js/login.js
@@ -10,3 +10,42 @@ var error = url.searchParams.get("error");
 if (error) {
   document.querySelector("#error").style = "";
 }
+
+// Theme-Selector
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('light')
+}else{
+    setTheme(theme)
+}
+
+let themeDots = document.querySelectorAll(".theme-dot")
+
+for (var i=0; themeDots.length > i; i++){
+    themeDots[i].addEventListener("click", function(){
+        let mode = this.dataset.mode
+        console.log('Option clicked:', mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    switch(mode){
+        case 'light':
+            document.querySelector('#theme-style').href = '../css/login.css'
+            break;
+        case 'blue':
+            document.querySelector('#theme-style').href = '../css/blue.css'
+            break;
+        case 'green':
+            document.querySelector('#theme-style').href = '../css/green.css'
+            break;
+        case 'purple':
+            document.querySelector('#theme-style').href = '../css/purple.css'
+            break;
+        default:
+            document.querySelector('#theme-style').href = '../css/login.css'
+    }
+    localStorage.setItem('theme', mode)
+}
